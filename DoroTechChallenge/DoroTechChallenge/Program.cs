06da452c35fc32a75a813e253269548fc4009ab0
ramon barbosa <ramonmfb777@gmail.com>
2022-11-17 19:39:02 -0300

    criando a aplicacao

---
+++ DoroTechChallenge/DoroTechChallenge/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DoroTech API",
+        Version = "v1",
+        Description = "API construída para o desafio da DoroTech",
+        Contact = new OpenApiContact
+        {
+            Name = "Ramon Barbosa",
+            Email = "ramonmfb777@gmail.com",
+            Url = new Uri("https://github.com/ramonfbarbosa")
+        },
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Por favor utilize Bearer <TOKEN>",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
