e885106943e44ba40a8adea725311a1d0a5964f4
MuriloAredes <murilohenrique467@gmail.com>
2022-11-17 12:00:07 -0300

    compare password

--- bookstoreManager/BookstoreManager.Domain/dto/authenticationDto/LoginRequest.cs
+++ bookstoreManager/BookstoreManager.Domain/dto/authenticationDto/LoginRequest.cs
@@ -1,8 +1,14 @@
-﻿namespace BookstoreManager.Domain.dto.authenticationDto
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreManager.Domain.dto.authenticationDto
 {
     public class LoginRequest
     {
         public string Email { get; set; }= string.Empty;
         public string Password{ get; set; }= string.Empty;
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password:")]
+        public string ConfirmPassword{ get; set; }= string.Empty;
     }
 }
