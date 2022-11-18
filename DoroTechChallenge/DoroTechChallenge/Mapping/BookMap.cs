b0aa3a222e7c97162c408d21f54b937d91bd0552
ramon barbosa <ramonmfb777@gmail.com>
2022-11-17 21:28:26 -0300

    criando entidade book e mapeando no banco

---
+++ DoroTechChallenge/DoroTechChallenge/Mapping/BookMap.cs
@@ -0,0 +1,46 @@
+﻿using DoroTechChallenge.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoroTechChallenge.Mapping;
+
+public class BookMap : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.ToTable("TAB_LIVROS");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .HasColumnName("ID")
+            .HasColumnType("INT");
+        builder.Property(x => x.Name)
+            .HasColumnName("NOME")
+            .HasColumnType("VARCHAR(30)")
+            .IsRequired();
+        builder.Property(x => x.Description)
+            .HasColumnName("DESCRICACO")
+            .HasColumnType("VARCHAR(MAX)")
+            .IsRequired();
+        builder.Property(x => x.Genre)
+            .HasColumnName("GENERO")
+            .HasColumnType("VARCHAR(30)")
+            .IsRequired();
+        builder.Property(x => x.Author)
+            .HasColumnName("AUTOR")
+            .HasColumnType("VARCHAR(30)")
+            .IsRequired();
+        builder.Property(x => x.PublishingCompany)
+            .HasColumnName("EDITORA")
+            .HasColumnType("VARCHAR(30)")
+            .IsRequired();
+        builder.Property(x => x.Active)
+            .HasColumnName("ATIVO")
+            .HasColumnType("BIT")
+            .IsRequired();
+        builder.Property(x => x.PublishedAt)
+            .HasColumnName("DATA_PUBLICACAO")
+            .HasColumnType("VARCHAR(30)")
+            .IsRequired();
+    }
+}
---
+++ DoroTechChallenge/DoroTechChallenge/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace DoroTechChallenge.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Genre { get; set; }
+    public string Author { get; set; }
+    public string PublishingCompany { get; set; }
+    public bool Active { get; set; }
+    public DateTime PublishedAt { get; set; }
+}
