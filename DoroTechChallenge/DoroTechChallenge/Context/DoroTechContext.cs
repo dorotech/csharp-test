3648a2d3afa3261347cb770253adf1081d62d8e9
ramon barbosa <ramonmfb777@gmail.com>
2022-11-17 21:31:27 -0300

    criando o context da aplicacao

---
+++ DoroTechChallenge/DoroTechChallenge/Context/DoroTechContext.cs
@@ -0,0 +1,19 @@
+﻿using DoroTechChallenge.Mapping;
+using DoroTechChallenge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoroTechChallenge.Context;
+
+public class DoroTechContext : DbContext
+{
+    public virtual DbSet<Book> Books { get; set; }
+
+    public DoroTechContext() { }
+
+    public DoroTechContext(DbContextOptions<DoroTechContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new BookMap());
+    }
+}
