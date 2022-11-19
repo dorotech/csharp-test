774a88ecabcfe28af29e85a75ae5f2b813520ae7
ramon barbosa <ramonmfb777@gmail.com>
2022-11-19 17:52:07 -0300

    adicionando codigo de paginacao

---
+++ DoroTechChallenge/DoroTechChallenge/Pagination/IQueryableExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Kumbajah.Services.Pagination;
+
+public static class IQueryableExtension
+{
+    public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PagedResponse pagination)
+    {
+        if (pagination == null || pagination.ItemsPerPage == 0 || pagination.PageNumber == 0)
+        {
+            return queryable;
+        }
+        var skip = (pagination.PageNumber - 1) * pagination.ItemsPerPage;
+        return queryable.Skip(skip).Take(pagination.ItemsPerPage);
+    }
+}
---
+++ DoroTechChallenge/DoroTechChallenge/Pagination/PaginationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Kumbajah.Services.Pagination;
+
+public class PaginationResponse<T> where T : class
+{
+    public int ResultsCount { get; set; }
+    public List<T> Results { get; set; }
+
+    public PaginationResponse(int resultCount, List<T> results)
+    {
+        ResultsCount = resultCount;
+        Results = results;
+    }
+}
