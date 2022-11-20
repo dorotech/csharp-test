8e1a9dd71bf36df9fb27aa4a49902633bc9be018
ramon barbosa <ramonmfb777@gmail.com>
2022-11-20 03:46:41 -0300

    consertando update e delete do bookRepository

--- DoroTechChallenge/DoroTechChallenge/Repositories/BookRepository.cs
+++ DoroTechChallenge/DoroTechChallenge/Repositories/BookRepository.cs
@@ -20,11 +20,11 @@ public class BookRepository : IBookRepository
         .Include(x => x.PublishingCompanies)
         .Include(x => x.Genre);
 
-    public Book FindByName(string bookName) =>
+    public Book FindByTitle(string bookName) =>
             BooksWithInclude().FirstOrDefault(x => x.Title.ToLower() == bookName.ToLower());
 
     public Book FetchBook(int id) =>
-        Context.Books.AsNoTracking()
+        BooksWithInclude().AsNoTracking()
             .FirstOrDefault(x => x.Id == id);
 
     public IQueryable<Book> Filter(Filter filter)
@@ -57,17 +57,27 @@ public class BookRepository : IBookRepository
         }
     }
 
-    public async Task<Book> UpdateAsync(Book book)
+    public async Task<Tuple<bool, Book>> TryUpdate(Book book)
     {
         using var transaction = Context.Database.BeginTransaction();
         try
         {
-            var attachedBook = Context.Attach(book);
-            attachedBook.State = EntityState.Modified;
-            var proxy = Context.Books.Update(book);
-            await Context.SaveChangesAsync();
-            transaction.Commit();
-            return proxy.Entity;
+            var existingBook = FetchBook(book.Id);
+            if (existingBook is not null)
+            {
+                var proxy = Context.Books.Attach(book);
+                proxy.State = EntityState.Modified;
+                Context.Update(book);
+                await Context.SaveChangesAsync();
+                transaction.Commit();
+                return Tuple.Create(true, proxy.Entity);
+            }
+            else
+            {
+                await Context.SaveChangesAsync();
+                transaction.Commit();
+                return Tuple.Create(false, book);
+            }
         }
         catch (Exception)
         {
@@ -80,7 +90,7 @@ public class BookRepository : IBookRepository
         }
     }
 
-    public async Task<bool> TryDelete(int id)
+    public async Task<Tuple<bool, int>> TryDelete(int id)
     {
         using var transaction = Context.Database.BeginTransaction();
         try
@@ -88,12 +98,12 @@ public class BookRepository : IBookRepository
             var book = FetchBook(id);
             if (book is not null)
             {
-                var proxy = Context.Books.Add(book);
+                var proxy = Context.Books.Remove(book);
                 await Context.SaveChangesAsync();
                 transaction.Commit();
-                return true;
+                return Tuple.Create(true, proxy.Entity.Id);
             }
-            return false;
+            return Tuple.Create(false, 0);
         }
         catch (Exception)
         {
--- DoroTechChallenge/DoroTechChallenge/Repositories/IBookRepository.cs
+++ DoroTechChallenge/DoroTechChallenge/Repositories/IBookRepository.cs
@@ -6,9 +6,9 @@ namespace DoroTechChallenge.Repositories;
 public interface IBookRepository
 {
     Book FetchBook(int id);
-    Book FindByName(string book);
-    Task<Book> InsertAsync(Book book);
-    Task<Book> UpdateAsync(Book book);
-    Task<bool> TryDelete(int id);
+    Book FindByTitle(string book);
     IQueryable<Book> Filter(Filter filter);
+    Task<Book> InsertAsync(Book book);
+    Task<Tuple<bool, Book>> TryUpdate(Book book);
+    Task<Tuple<bool, int>> TryDelete(int id);
 }
