496912beaa3715e3ddd654e2f53a035f4ef9ed34
Dev-Yan <yandevwork@gmail.com>
2023-02-18 07:46:25 -0300

    fix: add rule to include bearear token auth for endpoints

--- Startup.cs
+++ Startup.cs
@@ -55,6 +55,31 @@ namespace api
                         Email = Configuration.GetSection("CONTACT_EMAIL").Value
                     }
                 });
+                
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
 
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
